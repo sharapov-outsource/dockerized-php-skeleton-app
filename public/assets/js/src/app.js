b12874bcbc9b8a84006073b6d07c16b494427937
Alexander S <alexander@sharapov.biz>
2019-04-23 09:44:30 +0600

    adding basic modal windows, improving css

--- public/assets/js/src/app.js
+++ public/assets/js/src/app.js
@@ -1,6 +1,8 @@
 (function () {
-  var __AppModals = {};
-  var __Body = '';
+  var __AppModals = {},
+      __AppModalsClose = {},
+      __AppModalsClass = 'l--modal',
+      __Body = '';
 
   function __bindClick(elems, callback) {
     for (i = 0, l = elems.length; i < l; i++) {
@@ -8,12 +10,32 @@
     }
   }
 
-  document.addEventListener("DOMContentLoaded", function (event) {
+  function __findClosest(el, sel) {
+    while ((el = el.parentElement) && !((el.matches || el.matchesSelector).call(el, sel))) ;
+    return el;
+  }
+
+  function __countOpenedModals() {
+    _m = document.querySelectorAll('.' + __AppModalsClass);
+    _v = 0;
+    if (_m) {
+      for (i = 0, l = _m.length; i < l; i++) if (__visible(_m[i])) _v++;
+    }
+    return _v;
+  }
+
+  function __visible(e) {
+    return !!(e.offsetWidth || e.offsetHeight || e.getClientRects().length);
+  }
+
+  function __initModals() {
     __AppModals = document.querySelectorAll('[data-toggle="modal"]');
+    __AppModalsClose = document.querySelectorAll('.' + __AppModalsClass + '-close');
     __Body = document.getElementsByTagName('body')[0];
     // Modals
     if (__AppModals.length > 0) {
       __bindClick(__AppModals, function (event) {
+        $__c = __countOpenedModals();
         if (typeof event.target.dataset.target == "undefined") {
           console.warn("No target popup defined");
         } else {
@@ -26,15 +48,29 @@
             console.warn("No target popup defined");
           } else {
             $__m.classList.add('open');
-            __Body.classList.add('l--modal-open');
+            $__m.style.zIndex = (($__c * 5) + 50);
+            __Body.classList.add(__AppModalsClass + '-open');
           }
-          /*__bindClick(event.target.getElementsByClassName('.l--modal-close'), function (event) {
-            console.log($__m);
-            $__m.classList.remove('open');
-          });*/
         }
       });
     }
+    // Binding close modal buttons
+    if (__AppModalsClose.length > 0) {
+      __bindClick(__AppModalsClose, function (event) {
+        $__p = __findClosest(event.target, '.' + __AppModalsClass);
+        if (!$__p) {
+          console.warn("No target popup defined");
+        } else {
+          $__p.classList.remove('open');
+          __Body.classList.remove(__AppModalsClass + '-open');
+          $__m.style.zIndex = '';
+        }
+      });
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", function (event) {
+    __initModals();
   });
 
 
--- public/index.php
+++ public/index.php
@@ -158,13 +158,31 @@ include '../vendor/autoload.php';
     <button class="l--btn" data-toggle="modal" data-target="#exampleModal">Open modal</button>
   </fieldset>
 </div>
+
+<div id="exampleInnerModal" class="l--modal">
+  <div class="l--modal-inner">
+    <div class="l--modal-header">
+      Inner demo modal window <button class="l--modal-close"></button>
+    </div>
+    <div class="l--modal-content">
+    <p>Lorem Ipsum is simply. <button
+          class="l--btn" data-toggle="modal" data-target="#exampleInner2Modal">Open modal 2</button></p>
+    </div>
+    <div class="l--modal-footer">
+      <button class="l--btn">Save</button><button class="l--btn l--modal-close">Close</button>
+    </div>
+  </div>
+</div>
+
 <div id="exampleModal" class="l--modal">
   <div class="l--modal-inner">
     <div class="l--modal-header">
       Demo modal window <button class="l--modal-close"></button>
     </div>
     <div class="l--modal-content">
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+      standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. <button
+          class="l--btn" data-toggle="modal" data-target="#exampleInnerModal">Open modal</button></p>
     <table class="l--table">
       <thead>
         <tr>
@@ -197,9 +215,38 @@ include '../vendor/autoload.php';
     </div>
   </div>
 </div>
-
-
-
-
+<div id="exampleInner2Modal" class="l--modal">
+  <div class="l--modal-inner">
+    <div class="l--modal-header">
+      Inner demo modal 2 window <button class="l--modal-close"></button>
+    </div>
+    <div class="l--modal-content">
+    <p>Lorem Ipsum is simply.</p>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+      standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+        typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+      standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+        typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+      standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+        typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+      standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+        typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+      standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+        typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+    <div class="l--modal-footer">
+      <button class="l--btn">Save</button><button class="l--btn l--modal-close">Close</button>
+    </div>
+  </div>
+</div>
 </body>
 </html>
